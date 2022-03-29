88959357476d15055d039e8fffd2eb34ba62db75
Julio.S <just.julio95@gmail.com>
2022-03-29 14:37:41 -0400

    created ten questions for MCU quiz

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+var questions = [
+    {
+        question: "What is the first line of dialogue in Iron Man, the very first MCU film?",
+        choices: ["I feel like you're driving me to a court-marshal.", "I am Iron Man", "Is it cool if I take a picture with you?"],
+        answer: "I feel like you're driving me to a court-marshal"
+    },
+    {
+        question: "What video game is Thor addicted to in Avengers: Endgame?",
+        choices: ["World of Warcraft", "Fortnite", "Rocket League"],
+        answer: "Fortnite"
+    },
+    {
+        question: "Which film did Black Panther first appear in?",
+        choices: ["Black Panther", "Captain America: Civil War", "Captain America: The Winter Soldier"],
+        answer: "Captain America: Civil War"
+    },
+    {
+        question: "Where does Ant-Man get stuck at the end of Ant-Man and the Wasp?",
+        choices: ["Wakanda", "The Quantum Realm", "In space"],
+        answer: "The Quantum Realm"
+    },
+    {
+        question: "In Captain Marvel, what kind of alien is Groose the Cat?",
+        choices: ["Chitauri", "Flerken", "Sakaaran"],
+        answer: "Flerken"
+    },
+    {
+        question: "What is the name of the amulet Dr. Strange wears around his neck?",
+        choices: ["The Orb of the Ancient One", "The Eye of Agamotto", "The Relic of Kamar-Taj"],
+        answer: "The Eye of Agamotto"
+    },
+    {
+        question: "What song does Baby Groot dance to at the end of Guardians of the Galaxy?",
+        choices: ["Ain't No Mountain High Enough - Marvin Gaye", "I Want You Back - Jackson 5", "Moonage Daydream - David Bowie"],
+        answer: "I Want You Back - Jackson 5"
+    },
+    {
+        question: "In the post-credit scene, what food do the Avengers eat after the Battle of New York?",
+        choices: ["Shawarma", "Chinese", "Sushi"],
+        answer: "Shawarma"
+    },
+    {
+        question: "The final battle between Spider-Man and Mysterio takes place on which bridge?",
+        choices: ["Waterloo Bridge", "London Bridge", "Tower Bridge"],
+        answer: ["London Bridge"]
+    },
+    {
+        question: "In Captain America: The First Avenger, what does Steve Rogers say when asked if he wants to kill Nazi's?",
+        choices: ["I'd kill and die for my country", "Who doesn't?", "I don't like bullies, I don't care where they're from"],
+        answer: "I don't like bullies, I don't care where they're from"
+    }
+];
+
+var points = 0;
+
+for (i = 0; i < questions.length; i++) {
+    var answer = prompt(questions[i].question);
+
+    if (answer.toLowerCase() === questions[i].answer.toLowerCase()) {
+        alert("CORRECT");
+        points += 10;
+        console.log(points);
+    }
+    else {
+        alert("WRONG");
+    }
+}
